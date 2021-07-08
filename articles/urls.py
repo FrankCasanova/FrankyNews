432f1476f653c1cd838cac93485d0769d745a1bb
Frank Casanova <frankcasanova.info@gmail.com>
2021-07-08 20:20:08 +0200

    all crud created

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,19 @@
-from django import path
-from .views import ArticleListView
+from django.urls import path
+from .views import (
+    ArticleListView,
+    ArticleUpdateView,  # new
+    ArticleDetailView,  # new
+    ArticleDeleteView,  # new
+    ArticleCreateView,  # new
+)
 
 urlpatterns = [
-    path('', ArticleListView.as_view(), name='articles')
+    path('<int:pk>/edit/',
+         ArticleUpdateView.as_view(), name='article_edit'),  # new
+    path('<int:pk>/',
+         ArticleDetailView.as_view(), name='article_detail'),  # new
+    path('<int:pk>/delete/',
+         ArticleDeleteView.as_view(), name='article_delete'),  # new
+    path('new/', ArticleCreateView.as_view(), name='article_new'),  # new
+    path('', ArticleListView.as_view(), name='article_list'),
 ]
--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('pages.urls')),
-    path('articles', include('articles.urls'))
+    path('articles/', include('articles.urls'))
 ]
