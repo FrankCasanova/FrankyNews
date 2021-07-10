b82e923b7204dc45ad9856a6d1fd67fe9354c658
frankcasanova <frankcasanova.info@gmail.com>
2021-07-10 15:29:36 +0200

    vale

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ env = Env()
 env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 
 # Quick-start development settings - unsuitable for production
